cba10169c70368c23a5e3a835667e628156afc72
wangying000 <1178922618@qq.com>
2016-10-12 16:33:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/task_27/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.dell.task_27;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent("android.intent.action.MY_BROADCAST");
+                intent.putExtra("broadcastmsg","hello receiver");
+                sendBroadcast(intent);
+            }
+        });
+
+    }
+
+
+    }
+
---
+++ app/src/main/java/com/example/dell/task_27/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.example.dell.task_27;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String strMsg=intent.getStringExtra("broadcastmsg");
+        Toast.makeText(context,strMsg,Toast.LENGTH_SHORT).show();
+
+
+    }
+}
